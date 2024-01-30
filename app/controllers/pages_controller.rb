c319ace8cc25deea1936603d01ab9c9adca6008a
Micokoko <mico.balina@gmail.com>
2024-01-31 00:16:52 +0800

    added adoption listing page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,9 @@
 class PagesController < ApplicationController
+    include Rails.application.routes.url_helpers
     def index
+    end
+
+    def show
+        @landing_page = landing_page_path
     end
 end
