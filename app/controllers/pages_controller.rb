6e3e1124976eae6c19b7e92b7b135a480b5739b4
Vive Velasco <48145286+DarkyRobin@users.noreply.github.com>
2024-02-02 13:34:35 +0800

    Revert "Breed details  (#1)" (#2)
    This reverts commit c4dbf3779dd528d4dbebeea36d5cff8b81f5f2aa.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,4 @@
 class PagesController < ApplicationController
-    include Rails.application.routes.url_helpers
     def index
-    end
-
-    def show
-        @landing_page = landing_page_path
     end
 end
