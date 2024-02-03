ee6972e4e3b7dea44ea57f4ffec1a990ca8625c9
DarkyRobin <velascoayen@gmail.com>
2024-02-03 12:23:01 +0800

    Added the table relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  #Associated to adoption_requests
+  has_many :adoption_requests
 end
