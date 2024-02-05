afee3cd884911da988c79968e0ba21c5676a61d8
DarkyRobin <velascoayen@gmail.com>
2024-02-05 20:20:42 +0800

    stash

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Custom route for showing individual dogs
-  get 'dogs/show/:id', to: 'dogs#show', as: :show_dog
 
-  # Resourceful route for dogs
+  # API endpoints
+  get 'dogs/breeds', to: 'dogs#breeds', as: 'breeds'
+  get 'dogs/image/:image_id', to: 'dogs#image_details', as: 'image_details'
   resources :dogs, only: [:show]
 
-  # Root path set to landing page
+  # Remove the session route and set the root path to the landing page
   root 'pages#index', as: :landing_page
 
   # Custom route for adoption listing viewing
