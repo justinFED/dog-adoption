2478dad28c7f1f3bf7b1c7173518c46be70490fb
Justin Cantillo <justin.tech96@gmail.com>
2024-02-08 12:07:35 +0800

    Fixing Dog Description

--- config/routes.rb
+++ config/routes.rb
@@ -1,57 +1,36 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users
 
-<<<<<<< HEAD
   resources :dogs, only: [:index, :new, :create, :edit, :update, :destroy]
 
   # API endpoints
   get 'dogs/breeds', to: 'dogs#breeds', as: 'breeds'
   get 'dogs/image/:image_id', to: 'dogs#image_details', as: 'image_details'
+  resources :dogs, only: [:show]
   get 'breeds/:breed_name/temperament', to: 'breeds#temperament', as: :breed_temperament
 
   resources :breeds do
     get 'temperament', on: :member
   end
-  
+
   # Remove the session route and set the root path to the landing page
-=======
-  # Custom route for showing individual dogs
-  get 'dogs/show/:id', to: 'dogs#show', as: :show_dog
-
-  # Resourceful route for dogs
-  resources :dogs, only: [:show]
-
-  # User routes
-  namespace :user do
-    resources :dashboard, only: [:index] do
-      # Define listings action under the dashboard namespace
-      get 'listings', on: :collection
-    end
-  end
-  
-
-  # Root path set to landing page
->>>>>>> admin
   root 'pages#index', as: :landing_page
 
   # Custom route for adoption listing viewing
-  get 'pages/show', to: 'pages#show', as: :adoption_listing_viewing
-
+  get 'pages/show' => 'pages#show', as: :adoption_listing_viewing
   # Admin dashboard routes
   namespace :admin do
     resources :dashboard, only: [:index]
     resources :users
   end
-<<<<<<< HEAD
 
   # User routes
   namespace :user do
+    resources :dashboard, only: [:index]
     resources :dashboard, only: [:index] do
       # Define listings action under the dashboard namespace
       get 'listings', on: :collection
     end
   end
-  
-=======
->>>>>>> admin
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2024_02_07_090706) do
-=======
-ActiveRecord::Schema[7.1].define(version: 2024_02_05_151902) do
->>>>>>> admin
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
