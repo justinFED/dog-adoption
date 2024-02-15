b9e0e671a97999ffc8565fea47c2d595891e74a8
Vive Velasco <48145286+DarkyRobin@users.noreply.github.com>
2024-02-15 17:37:41 +0800

    Adoption request Controller unit tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     first_name { "John" }
     last_name { "Doe" }
-    email { "john.doe@example.com" }
+    sequence(:email) { |n| "user#{n}@example.com" }
     password { "test123" }
   end
 end
