f467e9974594e6fff5efc54a9d7b159834dc25f9
DarkyRobin <velascoayen@gmail.com>
2024-02-21 00:45:10 +1100

    Transfer bg.mp4 to public asset

--- db/schema.rb
+++ db/schema.rb
@@ -68,7 +68,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_123411) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -78,6 +77,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_08_123411) do
     t.datetime "updated_at", null: false
     t.string "last_name"
     t.integer "role", default: 0, null: false
+    t.string "first_name"
     t.date "date_of_birth"
     t.string "address"
     t.string "contact_details"
