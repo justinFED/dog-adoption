04925a0e56b47974532da4f1e1f40c9951f07408
DarkyRobin <velascoayen@gmail.com>
2024-02-20 23:57:43 +1100

    Add seed record

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+admin = User.create!( email: 'admin@admin.com', password: 'admin123', password_confirmation: 'admin123', first_name: 'Admin', last_name: 'User', role: :admin )
